a00118c34471eee5db73490232e712a0ca9c36e5
jakew-o-0 <jakewbailey007@gmail.com>
2022-10-03 19:39:20 +0100

    all validation and file IO done and how many out done

--- Implementation/CountCoin.py
+++ Implementation/CountCoin.py
@@ -3,19 +3,30 @@ import tkinter.ttk
 
 class Logic:
     def __init__(self):
-        self.targetWeight = {"£2":120, "£1":175, "50p":160, "20p":250, "10p":325, "5p":235, "2p":356, "1p":356}
+        ########: dicts for weight, bag value and coin vlaue for each coin type
+        self.coinValue = {"£2":2, "£1":1, "50p":0.5, "20p":0.2, "10p":0.1, "5p":0.5, "2p":0.2, "1p":0.1}
+        self.weightPerCoin = {"£2":12, "£1":8.75, "50p":8, "20p":5, "10p":6.5, "5p":2.35, "2p":7.12, "1p":3.56}
+        self.bagValue = {"£2":20, "£1":20, "50p":10, "20p":10, "10p":5, "5p":5, "2p":1, "1p":1}
 
+
+
+    ########: checks if the inputed weight is the same as the target weight
+    ########: if true, the total correct bags is updated in both the totals dict and the volenteers dict
+    ########: other wise the the same is done without incrementing the correct totals
     def Validate_Data(self, coinType, coinWeight, name):
-        ########: checks if the inputed weight is the same as the target weight
-        ########: if true, the total correct bags is updated in both the totals dict and the volenteers dict
-        ########: other wise the the same is done without incrementing the correct totals
+
         try:
-            if(int(coinWeight) == self.targetWeight[coinType]):
+            targetWeight = int((self.bagValue[coinType] / self.coinValue[coinType]) * self.weightPerCoin[coinType])
+        except(ValueError):
+            return "The coin type of the bag is incorect or there is a mismatched coin."
+
+        try:
+            if(int(coinWeight) == targetWeight):
                 self.update_Totals(True, name)
                 return "Correct Bag"
             else:
                 self.update_Totals(False, name)
-                return "Incorect Bag"
+                return "Incorect Bag. {}".format(self.how_many_out(coinWeight, coinType))
 
         ########: input sanitation thangs        
         except(ValueError):
@@ -23,15 +34,21 @@ class Logic:
         except(KeyError):
             return "Please select a coin type"
     
+    
+
+    ########: when reading the file, the first line is a dictionary for the totals for every inputed data
+    ########: the second line, a list of dictionarys for each volenteer entered
+    ########: the two lines are each assigned to a variable to be eddited and writen back into the text file
+    ########: this specific func updates the totals dict  ===> {'Total': int, 'TotalBags': int, 'TotalCorrect': int, 'Accuracy': int}
     def update_Totals(self, correct, name):
         with open("CoinCount.txt", 'r') as coinCount:
             total = eval(coinCount.readline())
             volenteers = eval(coinCount.readline())
 
-        ########: updates the total dict
+        ########: updates the total dict incrementing each item -if needed
         total["Total"] += 1
         total["TotalBags"] += 1
-        total["Accuracy"] = (total["TotalCorrect"] / total["TotalBags"]) * 100
+        total["Accuracy"] = round((total["TotalCorrect"] / total["TotalBags"]) * 100, 2)
         if(correct):
             total["TotalCorrect"] +=1
 
@@ -41,7 +58,12 @@ class Logic:
         with open("./CoinCount.txt", 'w') as coinCount:
             coinCount.write(str(total) + "\n")
             coinCount.write(str(volenteers))
-    
+
+
+
+    ########: this specific func updates the volenteers dict ===> [{'name': str, 'TotalBags': int, 'TotalCorrect': int, 'Accuracy': int}]
+    ########: if there is already a volenteer by the same name, the dict will be updated
+    ########: otherwise, a new dict will be appended
     def update_Volenteers(self, volenteers, name, correct):
         if(correct):
             totalCorect = 1
@@ -54,14 +76,44 @@ class Logic:
             if(volenteers[i]["name"] == name):
                 volenteers[i]["TotalBags"] += 1
                 volenteers[i]["TotalCorrect"] += totalCorect
-                volenteers[i]["Accuracy"] = (int(volenteers[i]["TotalCorrect"]) / int(volenteers[i]["TotalBags"])) * 100
+                volenteers[i]["Accuracy"] = round((int(volenteers[i]["TotalCorrect"]) / int(volenteers[i]["TotalBags"])) * 100, 2)
                 return None
         
         volenteers.append(dict({"name":name, "TotalBags":1, "TotalCorrect":totalCorect, "Accuracy":accuracy}))
+
+
+
+    ########: calculates the diffrence between the number of expected coins and inputed coins
+    ########: then changes the output depending of if the answer is positive or negative, indicating coins needing to be added or taken away
+    def how_many_out(self, coinWeight, coinType):
+        out = int((int(coinWeight) / self.weightPerCoin[coinType]) - (self.bagValue[coinType] / self.coinValue[coinType]))
+
+        if(out > 0):
+            msg = "{} coins to be removed.".format(out)
+        else:
+            sout = str(out)
+            msg = "{} coins to be added.".format(sout[1:len(sout)])
+
+        return msg
+
+
+
         
+
+
+
+
+
 class Help_Page(tkinter.Frame):
     def __init__(self, root):
         tkinter.Frame.__init__(self, root)
+
+
+
+
+
+
+
 
 class TotalBags_Page(tkinter.Frame):
     def __init__(self, root):
@@ -74,6 +126,13 @@ class TotalBags_Page(tkinter.Frame):
         nameLabel.grid(column=0, row=0)
         totalBagsLabel.grid(column=1, row=0)
         acccuracyLabel.grid(column=2, row=0)
+
+
+
+
+
+
+
 
 class Total_Page(tkinter.Frame):
     def __init__(self, root):
@@ -95,6 +154,13 @@ class Total_Page(tkinter.Frame):
         bagsFrame.grid(column=1, row=0)
         bagsCorrect.grid(column=0, row=0)
         bagsAccuracy.grid(column=0, row=1)
+
+
+
+
+
+
+
 
 class Home_Page(tkinter.Frame):
     def __init__(self, root):
@@ -124,14 +190,22 @@ class Home_Page(tkinter.Frame):
         weightEntry.grid(column=1, row=1)
         self.outputLabel.grid(column=0, row=4, columnspan=2)
         self.coinTypeCB.grid(column=1, row=2)
-
         submitButton.grid(column=0, row=3, columnspan=2)
+
+
     
     def submitButton_Pressed(self):
         logic = Logic()
         outText = logic.Validate_Data(coinType=self.coinTypeCB.get(), coinWeight=self.weightSVar.get(), name=self.nameSVar.get())
         self.outputLabel.config(text=outText)
         
+
+
+
+
+
+
+
 class gui(tkinter.Tk):
     def __init__(self):
         super().__init__()
@@ -152,6 +226,8 @@ class gui(tkinter.Tk):
         fmenu.add_command(label="Quit", command=lambda: self.quit())
         menuBar.add_cascade(label="Options", menu=fmenu)
 
+
+
     ########: changes the curent frame
     ########: when called a new frame is given (nFrame), which is initialised as a child to the gui class
     ########: old frame is destroyed and new frame is packed
@@ -161,6 +237,13 @@ class gui(tkinter.Tk):
             self.currentFrame.destroy()
         self.currentFrame = newFrame
         self.currentFrame.pack(padx=5, pady=5)
+
+
+
+
+
+
+
 
 if(__name__ == "__main__"):
     gui = gui()
